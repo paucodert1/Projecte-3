98a2b8e3572e7fb4b13bac5759572ffed69c3fe1
SergiPB <94857445+SergiPB@users.noreply.github.com>
2022-05-03 19:38:01 +0200

    Add files via upload

--- Projecte3_BD.sql
+++ Projecte3_BD.sql
@@ -3,210 +3,210 @@ create database nwn;
 
 use nwn;
 
-CREATE TABLE `Monitors` (
-	`DNI` varchar(9) NOT NULL,
-	`Nom` varchar(20) NOT NULL,
-	`Cognom_1` varchar(20) NOT NULL,
-	`Cognom_2` varchar(20) NOT NULL,
-	`Tel` INT(9) NOT NULL,
-	`Data_naixement` DATE NOT NULL,
-	`Correu-e` varchar(255) NOT NULL,
-	PRIMARY KEY (`DNI`)
+CREATE TABLE `monitors` (
+	`dni` varchar(9) NOT NULL,
+	`nom` varchar(20) NOT NULL,
+	`cognom_1` varchar(20) NOT NULL,
+	`cognom_2` varchar(20) NOT NULL,
+	`tel` INT(9) NOT NULL,
+	`data_naixement` DATE NOT NULL,
+	`correu_e` varchar(255) NOT NULL,
+	PRIMARY KEY (`dni`)
 );
 
-CREATE TABLE `Cursos` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`Nom` varchar(20) NOT NULL,
-	`Descripcio` varchar(255) NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `cursos` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nom` varchar(20) NOT NULL,
+	`descripcio` varchar(255) NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Individual` (
-	`ID` INT NOT NULL,
-	`Preu/h` INT NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `individual` (
+	`id` INT NOT NULL,
+	`preu/h` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Colectiu` (
-	`ID` INT NOT NULL,
-	`Preu` INT NOT NULL,
-	`Max_participants` INT NOT NULL,
-	`Data` DATE NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `colectiu` (
+	`id` INT NOT NULL,
+	`preu` INT NOT NULL,
+	`max_participants` INT NOT NULL,
+	`data` DATE NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Competicio` (
-	`ID` INT NOT NULL,
-	`Preu` INT NOT NULL,
-	`Nivell` INT NOT NULL,
-	`Data_inici` DATE NOT NULL,
-	`Data_fi` DATE NOT NULL,
-	`Durada` DATETIME NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `competicio` (
+	`id` INT NOT NULL,
+	`preu` INT NOT NULL,
+	`nivell` INT NOT NULL,
+	`data_inici` DATE NOT NULL,
+	`data_fi` DATE NOT NULL,
+	`durada` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Clients` (
-	`DNI` varchar(9) NOT NULL,
-	`Nom` varchar(20) NOT NULL,
-	`Cognom_1` varchar(20) NOT NULL,
-	`Cognom_2` varchar(20) NOT NULL,
-	`Tel` INT(9) NOT NULL,
-	`Data_naixement` DATE NOT NULL,
-	`Correu-e` varchar(50) NOT NULL,
-	`Sexe` varchar(1) NOT NULL,
-	`Compte_Bancari` varchar(50) NOT NULL,
-	`Usuari` varchar(20) NOT NULL,
-	`Contrasenya` varchar(20) NOT NULL,
-	PRIMARY KEY (`DNI`)
+CREATE TABLE `clients` (
+	`dni` varchar(9) NOT NULL,
+	`nom` varchar(20) NOT NULL,
+	`cognom_1` varchar(20) NOT NULL,
+	`cognom_2` varchar(20) NOT NULL,
+	`tel` INT(9) NOT NULL,
+	`data_naixement` DATE NOT NULL,
+	`correu-e` varchar(50) NOT NULL,
+	`sexe` varchar(1) NOT NULL,
+	`compte_Bancari` varchar(50) NOT NULL,
+	`usuari` varchar(20) NOT NULL,
+	`contrasenya` varchar(32) NOT NULL,
+	PRIMARY KEY (`dni`)
 );
 
-CREATE TABLE `Federats` (
-	`DNI` varchar(9) NOT NULL,
-	`Num_Fed` INT(20) NOT NULL,
-	`Data_caduc` DATE NOT NULL,
-	`Nivell` INT NOT NULL,
-	PRIMARY KEY (`DNI`)
+CREATE TABLE `federats` (
+	`dni` varchar(9) NOT NULL,
+	`num_fed` INT(20) NOT NULL,
+	`data_caduc` DATE NOT NULL,
+	`nivell` INT NOT NULL,
+	PRIMARY KEY (`dni`)
 );
 
-CREATE TABLE `Curse_I` (
-	`ID` INT NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	`Hores` TIME NOT NULL,
-	`Data` DATE NOT NULL,
-	`Descomte/h` INT NOT NULL,
-	`Preu_final` INT NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `curse_i` (
+	`id` INT NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	`hores` TIME NOT NULL,
+	`data` DATE NOT NULL,
+	`descomte/h` INT NOT NULL,
+	`preu_final` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Curse_Col` (
-	`ID` INT NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	`Descomte_FN` INT NOT NULL,
-	`Preu_final` INT NOT NULL,
-	`Cant_persones` INT NOT NULL,
-	PRIMARY KEY (`ID`,`DNI`)
+CREATE TABLE `curse_col` (
+	`id` INT NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	`descomte_fn` INT NOT NULL,
+	`preu_final` INT NOT NULL,
+	`cant_persones` INT NOT NULL,
+	PRIMARY KEY (`id`,`dni`)
 );
 
-CREATE TABLE `Curse_Com` (
-	`ID` INT NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	`Preu_final` INT NOT NULL,
-	PRIMARY KEY (`ID`,`DNI`)
+CREATE TABLE `curse_com` (
+	`id` INT NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	`preu_final` INT NOT NULL,
+	PRIMARY KEY (`id`,`dni`)
 );
 
-CREATE TABLE `Familia_nombrosa` (
-	`DNI` varchar(9) NOT NULL,
-	`Carnet_FN` varchar(40) NOT NULL,
-	`Data_caduc` DATE NOT NULL,
-	PRIMARY KEY (`DNI`)
+CREATE TABLE `familia_nombrosa` (
+	`dni` varchar(9) NOT NULL,
+	`carnet_fn` varchar(40) NOT NULL,
+	`data_caduc` DATE NOT NULL,
+	PRIMARY KEY (`dni`)
 );
 
-CREATE TABLE `Productes` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`Marca` varchar(20) NOT NULL,
-	`Model` varchar(20) NOT NULL,
-	`Preu` INT NOT NULL,
-	`Descompte` INT NOT NULL default '0',
-	`Usos` INT NOT NULL DEFAULT '0',
-	`Estat` BOOLEAN NOT NULL DEFAULT true,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `productes` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`marca` varchar(20) NOT NULL,
+	`model` varchar(20) NOT NULL,
+	`preu` INT NOT NULL,
+	`descompte` INT NOT NULL default '0',
+	`usos` INT NOT NULL DEFAULT '0',
+	`estat` BOOLEAN NOT NULL DEFAULT true,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Botes` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`Talla` INT NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `botes` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`talla` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Pals` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`Alçada` INT NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `pals` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`alçada` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Esquís` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`Tipus` varchar(50) NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `esquís` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`tipus` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Kits` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`ID_P1` INT NOT NULL,
-	`ID_P2` INT NOT NULL,
-	`ID_P3` INT NOT NULL,
-	`Preu` INT NOT NULL,
-	`Usos` INT NOT NULL,
-	PRIMARY KEY (`ID`)
+CREATE TABLE `kits` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`id_p1` INT NOT NULL,
+	`id_p2` INT NOT NULL,
+	`id_p3` INT NOT NULL,
+	`preu` INT NOT NULL,
+	`usos` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Format` (
-	`ID_P` INT NOT NULL,
-	`ID_K` INT NOT NULL,
-	`Preu_Final` INT NOT NULL,
-	PRIMARY KEY (`ID_P`,`ID_K`)
+CREATE TABLE `format` (
+	`id_p` INT NOT NULL,
+	`id_k` INT NOT NULL,
+	`preu_final` INT NOT NULL,
+	PRIMARY KEY (`id_p`,`id_k`)
 );
 
-CREATE TABLE `Lloga_K` (
-	`ID` int NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	`Data` DATETIME NOT NULL,
-	`Data_tornada` DATETIME NOT NULL,
-	PRIMARY KEY (`ID`,`DNI`)
+CREATE TABLE `lloga_k` (
+	`id` int NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	`data` DATETIME NOT NULL,
+	`data_tornada` DATETIME NOT NULL,
+	PRIMARY KEY (`id`,`dni`)
 );
 
-CREATE TABLE `Lloga_P` (
-	`ID` INT NOT NULL,
-	`DNI` varchar(9) NOT NULL,
-	`Data` DATETIME NOT NULL,
-	`Preu_final` INT NOT NULL,
-	`Data_tornada` DATETIME NOT NULL,
-	PRIMARY KEY (`ID`,`DNI`)
+CREATE TABLE `lloga_p` (
+	`id` INT NOT NULL,
+	`dni` varchar(9) NOT NULL,
+	`data` DATETIME NOT NULL,
+	`preu_final` INT NOT NULL,
+	`data_tornada` DATETIME NOT NULL,
+	PRIMARY KEY (`id`,`dni`)
 );
 
 
-ALTER TABLE `Cursos` ADD CONSTRAINT `Cursos_fk0` FOREIGN KEY (`DNI`) REFERENCES `Monitors`(`DNI`);
+ALTER TABLE `cursos` ADD CONSTRAINT `cursos_fk0` FOREIGN KEY (`dni`) REFERENCES `monitors`(`dni`);
 
-ALTER TABLE `Individual` ADD CONSTRAINT `Individual_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `individual` ADD CONSTRAINT `individual_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Colectiu` ADD CONSTRAINT `Colectiu_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `colectiu` ADD CONSTRAINT `colectiu_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Competicio` ADD CONSTRAINT `Competició_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `competicio` ADD CONSTRAINT `competició_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Federats` ADD CONSTRAINT `Federats_fk0` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `federats` ADD CONSTRAINT `federats_fk0` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
-ALTER TABLE `Curse_I` ADD CONSTRAINT `Curse_I_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `curse_i` ADD CONSTRAINT `curse_i_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Curse_I` ADD CONSTRAINT `Curse_I_fk1` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `curse_i` ADD CONSTRAINT `curse_i_fk1` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
-ALTER TABLE `Curse_Col` ADD CONSTRAINT `Curse_Col_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `curse_col` ADD CONSTRAINT `curse_col_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Curse_Col` ADD CONSTRAINT `Curse_Col_fk1` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `curse_col` ADD CONSTRAINT `curse_col_fk1` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
-ALTER TABLE `Curse_Com` ADD CONSTRAINT `Curse_Com_fk0` FOREIGN KEY (`ID`) REFERENCES `Cursos`(`ID`);
+ALTER TABLE `curse_com` ADD CONSTRAINT `curse_com_fk0` FOREIGN KEY (`id`) REFERENCES `cursos`(`id`);
 
-ALTER TABLE `Curse_Com` ADD CONSTRAINT `Curse_Com_fk1` FOREIGN KEY (`DNI`) REFERENCES `Federats`(`DNI`);
+ALTER TABLE `curse_com` ADD CONSTRAINT `curse_com_fk1` FOREIGN KEY (`dni`) REFERENCES `federats`(`dni`);
 
-ALTER TABLE `Familia_nombrosa` ADD CONSTRAINT `Familia_nombrosa_fk0` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `familia_nombrosa` ADD CONSTRAINT `familia_nombrosa_fk0` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
-ALTER TABLE `Botes` ADD CONSTRAINT `Botes_fk0` FOREIGN KEY (`ID`) REFERENCES `Productes`(`ID`);
+ALTER TABLE `botes` ADD CONSTRAINT `botes_fk0` FOREIGN KEY (`id`) REFERENCES `productes`(`id`);
 
-ALTER TABLE `Pals` ADD CONSTRAINT `Pals_fk0` FOREIGN KEY (`ID`) REFERENCES `Productes`(`ID`);
+ALTER TABLE `pals` ADD CONSTRAINT `pals_fk0` FOREIGN KEY (`id`) REFERENCES `productes`(`id`);
 
-ALTER TABLE `Esquís` ADD CONSTRAINT `Esquís_fk0` FOREIGN KEY (`ID`) REFERENCES `Productes`(`ID`);
+ALTER TABLE `esquís` ADD CONSTRAINT `esquís_fk0` FOREIGN KEY (`id`) REFERENCES `productes`(`id`);
 
-ALTER TABLE `Format` ADD CONSTRAINT `Format_fk0` FOREIGN KEY (`ID_P`) REFERENCES `Productes`(`ID`);
+ALTER TABLE `format` ADD CONSTRAINT `format_fk0` FOREIGN KEY (`id_p`) REFERENCES `productes`(`id`);
 
-ALTER TABLE `Format` ADD CONSTRAINT `Format_fk1` FOREIGN KEY (`ID_K`) REFERENCES `Kits`(`ID`);
+ALTER TABLE `format` ADD CONSTRAINT `format_fk1` FOREIGN KEY (`id_k`) REFERENCES `kits`(`id`);
 
-ALTER TABLE `Lloga_K` ADD CONSTRAINT `Lloga_K_fk0` FOREIGN KEY (`ID`) REFERENCES `Kits`(`ID`);
+ALTER TABLE `lloga_k` ADD CONSTRAINT `lloga_k_fk0` FOREIGN KEY (`id`) REFERENCES `kits`(`id`);
 
-ALTER TABLE `Lloga_K` ADD CONSTRAINT `Lloga_K_fk1` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `lloga_k` ADD CONSTRAINT `lloga_k_fk1` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
-ALTER TABLE `Lloga_P` ADD CONSTRAINT `Lloga_P_fk0` FOREIGN KEY (`ID`) REFERENCES `Productes`(`ID`);
+ALTER TABLE `lloga_p` ADD CONSTRAINT `lloga_p_fk0` FOREIGN KEY (`id`) REFERENCES `productes`(`id`);
 
-ALTER TABLE `Lloga_P` ADD CONSTRAINT `Lloga_P_fk1` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
+ALTER TABLE `lloga_p` ADD CONSTRAINT `lloga_p_fk1` FOREIGN KEY (`dni`) REFERENCES `clients`(`dni`);
 
 
 
@@ -215,17 +215,17 @@ ALTER TABLE `Lloga_P` ADD CONSTRAINT `Lloga_P_fk1` FOREIGN KEY (`DNI`) REFERENCE
 #################
 
 # Monitors #
-insert into Monitors value ("78103615P", "Sergi", "Pont", "Balague", 600382677, 25/08/2003, "sergipb@gmail.com");
-insert into Monitors value ("12345678J", "Marta", "Moreno", "Rubio", 643125476, 06/04/2000, "martamr@gmail.com");
-insert into Monitors value ("56864844G", "Jordi", "Estnañ", "Font", 684212354, 18/10/1990, "jordief@gmail.com");
+insert into monitors value ("78103615P", "Sergi", "Pont", "Balague", 600382677, 25/08/2003, "sergipb@gmail.com");
+insert into monitors value ("12345678J", "Marta", "Moreno", "Rubio", 643125476, 06/04/2000, "martamr@gmail.com");
+insert into monitors value ("56864844G", "Jordi", "Estnañ", "Font", 684212354, 18/10/1990, "jordief@gmail.com");
 
 
 # Cursos #
-insert into Cursos value (1, "Inicial", "Curs per a nous usuaris sense cap experiencia", "78103615P");
-insert into Cursos value (default, "Desens Basic", "Curs per apendre a baixa corectament", "56864844G");
-insert into Cursos value (default, "Mitg", "Curs per usuaris amb experiencia basica", "78103615P");
-insert into Cursos value (default, "Max velocitat", "Curs per aconseguir la maxima velocitat en vaixada", "12345678J");
-insert into Cursos value (default, "Acrobacies Basiques", "Curs per comensa amb acrobasies", "56864844G");
+insert into cursos value (1, "Inicial", "Curs per a nous usuaris sense cap experiencia", "78103615P");
+insert into cursos value (default, "Desens Basic", "Curs per apendre a baixa corectament", "56864844G");
+insert into cursos value (default, "Mitg", "Curs per usuaris amb experiencia basica", "78103615P");
+insert into cursos value (default, "Max velocitat", "Curs per aconseguir la maxima velocitat en vaixada", "12345678J");
+insert into cursos value (default, "Acrobacies Basiques", "Curs per comensa amb acrobasies", "56864844G");
 
 	# Individual #
 	insert into individual value (1, 50);
@@ -241,10 +241,10 @@ insert into Cursos value (default, "Acrobacies Basiques", "Curs per comensa amb 
 
 # Clients #
 
-insert into clients value ("15423675U", "Pau", "Rubio", "Silva", 62378912, 21/03/2003, "paurubiosilva@gmail.com", "M", "ES25 0019 0832 4975 2231 4577", "Prubio", "1234");
-insert into clients value ("94941664R", "Sergi", "Folorencia", "Rotj", 624879216, 03/02/2003, "sergifr@gmail.com", "M", "ES28 6814 4959 7380 2145 2793", "UwU", "1234");
-insert into clients value ("51515482D", "Paula", "Sanchez", "Canosa", 679123574, 19/11/2001, "paulasc@gmail.com", "F", "ES26 0149 3779 6978 6983 9149", "Paulita", "1234");
-insert into clients value ("81664477H", "Paco", "Gimenez", "Gerra", 634897123, 07/05/1970, "pacogg@gmail.com", "M", "ES81 0077 9275 2263 3174 6967", "PacoGG", "1234");
+insert into clients value ("15423675U", "Pau", "Rubio", "Silva", 62378912, 21/03/2003, "paurubiosilva@gmail.com", "M", "ES25 0019 0832 4975 2231 4577", "Prubio", md5("1234"));
+insert into clients value ("94941664R", "Sergi", "Folorencia", "Rotj", 624879216, 03/02/2003, "sergifr@gmail.com", "M", "ES28 6814 4959 7380 2145 2793", "UwU", md5("1234"));
+insert into clients value ("51515482D", "Paula", "Sanchez", "Canosa", 679123574, 19/11/2001, "paulasc@gmail.com", "F", "ES26 0149 3779 6978 6983 9149", "Paulita", md5("1234"));
+insert into clients value ("81664477H", "Paco", "Gimenez", "Gerra", 634897123, 07/05/1970, "pacogg@gmail.com", "M", "ES81 0077 9275 2263 3174 6967", "PacoGG", md5("1234"));
 
 	# Federat #
     
@@ -252,34 +252,34 @@ insert into clients value ("81664477H", "Paco", "Gimenez", "Gerra", 634897123, 0
     
     # Familia Nombrosa #
     
-	insert into Familia_nombrosa value ("81664477H", "69464616476", 22/04/2024);
+	insert into familia_nombrosa value ("81664477H", "69464616476", 22/04/2024);
     
     
 # Curse_I #
 
-insert into Curse_I value (1, "94941664R", "04:00:00", 14/01/2022, 30, 140);
-insert into Curse_I value (3, "15423675U", "07:00:00", 14/01/2022, 50, 175);
+insert into curse_i value (1, "94941664R", "04:00:00", 14/01/2022, 30, 140);
+insert into curse_i value (3, "15423675U", "07:00:00", 14/01/2022, 50, 175);
 
 # Curse_Col #
 
-insert into Curse_Col value (2, "81664477H", 40, 108, 6);
-insert into Curse_Col value (2, "15423675U", 0, 90, 3);
+insert into curse_col value (2, "81664477H", 40, 108, 6);
+insert into curse_col value (2, "15423675U", 0, 90, 3);
 
 # Curse_Com #
 
-insert into Curse_com value (4, "51515482D", 60);
+insert into curse_com value (4, "51515482D", 60);
 
 # Productes #
 
-insert into Productes value (1, "Salomon", "MAX 8", 50, default, 2, default);
-insert into Productes value (default, "Atomic", "REDSTER X5", 70, default, default, default);
-insert into Productes value (default, "Wedze", "BOOST 580", 40, default, 1, default);
-insert into Productes value (default, "Salomon", "QUEST ACCESS 70", 20, default, 1, default);
-insert into Productes value (default, "Fischer", "Travers TS", 50, default, 2, default);
-insert into Productes value (default, "Wedze", "WID 300", 15, default, default, default);
-insert into Productes value (default, "Kerma", "Elite 2", 15, default, 2, default);
-insert into Productes value (default, "Inovik", "POLE 900", 20, default, default, default);
-insert into Productes value (default, "Wedze", "BOOST 500", 10, default, 1, default);
+insert into productes value (1, "Salomon", "MAX 8", 50, default, 1, default);
+insert into productes value (default, "Atomic", "REDSTER X5", 70, default, default, default);
+insert into productes value (default, "Wedze", "BOOST 580", 40, default, 1, default);
+insert into productes value (default, "Salomon", "QUEST ACCESS 70", 20, default, 1, default);
+insert into productes value (default, "Fischer", "Travers TS", 50, default, 1, default);
+insert into productes value (default, "Wedze", "WID 300", 15, default, default, default);
+insert into productes value (default, "Kerma", "Elite 2", 15, default, 1, default);
+insert into productes value (default, "Inovik", "POLE 900", 20, default, default, default);
+insert into productes value (default, "Wedze", "BOOST 500", 10, default, 1, default);
 
 	# Esquís #
     
@@ -289,35 +289,35 @@ insert into Productes value (default, "Wedze", "BOOST 500", 10, default, 1, defa
     
     # Botes #
     
-    insert into esquís value (4, 40);
-    insert into esquís value (5, 38);
-    insert into esquís value (6, 36);
+    insert into botes value (4, 40);
+    insert into botes value (5, 38);
+    insert into botes value (6, 36);
     
     # Pals #
     
-    insert into esquís value (7, "120");
-    insert into esquís value (8, "130");
-    insert into esquís value (9, "110");
+    insert into pals value (7, "120");
+    insert into pals value (8, "130");
+    insert into pals value (9, "110");
     
 # Kits #
 
-insert into kits values (1, 1, 5, 7, 115, 2);
+insert into kits values (1, 1, 5, 7, 115, 1);
 
 # Format #
 
-insert into Format values (1, 1, 50);
-insert into Format values (5, 1, 50);
-insert into Format values (7, 1, 15);
+insert into format values (1, 1, 50);
+insert into format values (5, 1, 50);
+insert into format values (7, 1, 15);
 
 # Lloga_K #
     
-insert into Lloga_K values (1, "15423675U", "2022/01/14 11:00:00", "2022/01/14 19:00:00");
+insert into lloga_k values (1, "15423675U", "2022/01/14 11:00:00", "2022/01/14 19:00:00");
 
 # Lloga_P #
 
-insert into Lloga_P values (3,"94941664R", "2022/01/14 15:00:00", 40, "2022/01/14 19:00:00");
-insert into Lloga_P values (9,"94941664R", "2022/01/14 15:00:00", 10, "2022/01/14 19:00:00");
-insert into Lloga_P values (4,"81664477H", "2022/01/05 15:00:00", 20, "2022/01/05 19:00:00");      
+insert into lloga_p values (3,"94941664R", "2022/01/14 15:00:00", 40, "2022/01/14 19:00:00");
+insert into lloga_p values (9,"94941664R", "2022/01/14 15:00:00", 10, "2022/01/14 19:00:00");
+insert into lloga_p values (4,"81664477H", "2022/01/05 15:00:00", 20, "2022/01/05 19:00:00");      
     
 
 
